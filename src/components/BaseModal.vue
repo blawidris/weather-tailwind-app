1b6842b8ac7c1f78313741162f79fde9b72fdb59
blawidris <blawidris@gmail.com>
2023-05-20 11:09:03 -0700

    creating usable modal, navbar, city search box and fetching hand handling api

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,68 @@
+<template>
+
+    <Teleport to="body">
+        <Transition name="modal-outer">
+            <div v-show="modalActive" class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
+                <Transition name="modal-inner">
+                    <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md">
+                        <button class="text-white mb-9 bg-weather-primary py-2 px-6 flex ml-auto" @click="$emit('close-modal')"><i class="fa-solid fa-times"></i></button>
+                        <slot/>
+                    </div>
+                </Transition>
+             </div>
+        </Transition>
+    </Teleport>
+    
+</template>
+
+<script>
+
+export default {
+    props: {
+        modalActive: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    emits: ['close-modal']
+}
+
+// defineProps({
+//     modalActive: {
+    // type: Boolean,
+    //     default: false,
+//     }
+// });
+
+// defineEmits(['close-modal'])
+
+
+</script>
+
+
+<style scoped>
+.modal-outer-enter-active, .modal-outer-leave-active{
+    transition: opacity 0.3s cubic-bezier(.52, 0.02, 0.19, 1.02);
+}
+
+.modal-outer-enter-from, .modal-outer-leave-to{
+    opacity: 0;
+}
+
+.modal-inner-enter-active{
+    transition: all 0.3s cubic-bezier(.52, 0.02, 0.19, 1.02) 0.15s;
+}
+.modal-inner-leave-active{
+    transition: all 0.3s cubic-bezier(.52, 0.02, 0.19, 1.02);
+}
+
+.modal-inner-enter-from{
+    opacity: 0;
+    transform: scale(.8);
+}
+
+.modal-inner-leave-to{
+    transform: scale(.8);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- container -->
+  <section id="home" class="container text-white">
+    <div class="p-4 mb-8 relative">
+      <input type="text" placeholder="Search for a city or state" v-model="searchQuery" @input="getSearchQuery"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+      <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]" v-if="mapboxResults">
+        <p class="px-1 text-warning" v-if="searchErr">Sorry, something went wrong please try again later</p>
+        <p class="px-1 text-warning" v-if="!searchErr && mapboxResults.length === 0">No result match your query, try a
+          different term</p>
+        <template v-else>
+          <li class="py-2 cursor-pointer" v-for="searchResult in mapboxResults" :key="searchResult.id">
+            {{ searchResult.place_name }}
+          </li>
+        </template>
+
+      </ul>
+    </div>
+
+
+
+  </section>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+<script setup>
+
+import { ref } from 'vue'
+import axios from 'axios'
+
+const mapboxAPIkey = 'pk.eyJ1IjoiYmxhd2lkcmlzIiwiYSI6ImNsaHczMHZnZzBmdGUzaHJ3YjRxYXc4NDIifQ.dK-S5573wS-0Yr4Umh10Wg'
+
+const searchQuery = ref(null)
+const queryTimeout = ref(null)
+const mapboxResults = ref(null)
+const searchErr = ref(null)
+
+const getSearchQuery = () => {
+
+  clearTimeout(queryTimeout.value);
+
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== null) {
+
+      try {
+        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIkey}&types=place`)
+
+        mapboxResults.value = result.data.features;
+
+      } catch {
+        searchErr.value = true
+        console.log(searchErr)
+      }
+
+    }
+    mapboxResults.value = null;
+    return;
+  }, 300);
 }
+
 </script>
